ea31d57daa9024f99d70a2135f1d5e35725b203b
Brown_ Alexander James <alexander.brown@login-p3n04.kamiak.wsu.edu>
2025-04-04 18:05:54 -0700

    feat: code to make custom blast db, prep db for diamond, and test diamond blastp

--- src/app/annotation/database_prep.sh
+++ src/app/annotation/database_prep.sh
@@ -9,4 +9,8 @@ extract-taxon-protein-sequences() {
 	blastdbcmd -db nr -taxids $1 -out $2
 }
 
+make_protein_blast_db() {
+	makeblastdb -in $1 -dbtype prot -title $2 -parse_seqids -blastdb_version 5 -taxid $3 -out $4
+}
+
 "$@"
