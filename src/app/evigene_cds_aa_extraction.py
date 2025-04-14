777b29cfbccc54371ec68ca0c423ee77adb3f469
Brown_ Alexander James <alexander.brown@login-p3n04.kamiak.wsu.edu>
2025-04-14 14:38:22 -0700

    feat: added parameter to add gene name to fasta file

--- src/app/evigene_cds_aa_extraction.py
+++ src/app/evigene_cds_aa_extraction.py
@@ -20,7 +20,7 @@ class CdsAaFastaManager:
         with json_path.open() as inhandle:
             return load(inhandle)
 
-    def run(self, codon_to_aa: dict[str]=None) -> None:
+    def run(self, codon_to_aa: dict[str]=None, add_gene_name: True=None) -> None:
         # Extract a dictionary of transcript ids mapping to cds ids (sometimes more than one cds id per transcript id)
         transcript_cds_id_mapping = self.extract_transcript_cds_id_mapping(self.transcript_metadata, self.extraction_fields)
         print(len(transcript_cds_id_mapping))
@@ -32,18 +32,23 @@ class CdsAaFastaManager:
             transcript_cds_id_mapping = self.filter_transcript_cds_id_mapping(transcript_cds_id_mapping, filtered_cds_ids)
             print(len(transcript_cds_id_mapping))
 
+        if add_gene_name:
+            cds_gene_mapping = self.extract_cds_gene_mapping(self.cds_metadata)
+        else:
+            cds_gene_mapping = None
+
         # Extract cds_position info
         cds_positions = self.extract_cds_positions(self.cds_metadata)
         print(len(cds_positions))
 
         # Using the remaining transcript ids write cds and/or aa fasta files
         if (self.cds_fasta) and (not self.aa_fasta):
-            self.extract_and_write_cds(transcript_cds_id_mapping, cds_positions, self.assembly_fasta, self.cds_fasta)
+            self.extract_and_write_cds(transcript_cds_id_mapping, cds_positions, self.assembly_fasta, self.cds_fasta, cds_gene_mapping)
         elif (not self.cds_fasta) and (self.aa_fasta):
-            self.extract_and_write_aa(transcript_cds_id_mapping, cds_positions, self.assembly_fasta, self.aa_fasta, codon_to_aa)
+            self.extract_and_write_aa(transcript_cds_id_mapping, cds_positions, self.assembly_fasta, self.aa_fasta, codon_to_aa, cds_gene_mapping)
         elif self.cds_fasta and self.aa_fasta:
             self.extract_and_write_cds_aa(transcript_cds_id_mapping, cds_positions, self.assembly_fasta,
-                                          self.cds_fasta, self.aa_fasta, codon_to_aa)
+                                          self.cds_fasta, self.aa_fasta, codon_to_aa, cds_gene_mapping)
         else:
             raise Exception("Neither output cds nor aa fasta paths detected\nNo calculation performed")
 
@@ -108,6 +113,19 @@ class CdsAaFastaManager:
         return filtered_transcript_cds_id_mapping
 
     @staticmethod
+    def extract_cds_gene_mapping(cds_metadata: Path) -> dict[str]:
+        print("Starting cds to gene name mapping")
+        cds_gene_mapping = dict()
+
+        with cds_metadata.open() as inhandle:
+            reader = DictReader(inhandle)
+            for data in reader:
+                cds_id = data["cds_id"]
+                gene = data["gene"]
+                cds_gene_mapping[cds_id] = gene
+        return cds_gene_mapping
+
+    @staticmethod
     def extract_cds_positions(cds_metadata: Path) -> dict[Any]:
         print("Starting cds positions extraction")
         cds_positions = dict()
@@ -127,7 +145,7 @@ class CdsAaFastaManager:
 
     @classmethod
     def extract_and_write_cds(cls, transcript_cds_id_mapping: dict[list[str]], cds_positions: dict[Any],
-                              assembly_fasta:Path, cds_fasta: Path) -> None:
+                              assembly_fasta:Path, cds_fasta: Path, cds_gene_mapping: Union[None, dict[str]]) -> None:
         print("Starting cds extraction and writing\n(This may take awhile)")
         with cds_fasta.open("w") as cds_outhandle:
             for fasta_seq in cls.fasta_chunker(assembly_fasta):
@@ -141,7 +159,11 @@ class CdsAaFastaManager:
 
                 for cds_id in cds_ids:
                     cds_seq = cls.extract_cds_sequence(cds_positions[cds_id], transcript_seq)
-                    cds_outhandle.write(f">{cds_id}\n")
+                    if cds_gene_mapping:
+                        gene = cds_gene_mapping[cds_id]
+                        cds_outhandle.write(f">{cds_id};{gene}\n")
+                    else:
+                        cds_outhandle.write(f">{cds_id}\n")
                     cds_outhandle.write(f"{cds_seq}\n")
 
     @staticmethod
@@ -181,7 +203,8 @@ class CdsAaFastaManager:
 
     @classmethod
     def extract_and_write_aa(cls, transcript_cds_id_mapping: dict[list[str]], cds_positions: dict[Any],
-                              assembly_fasta:Path, aa_fasta: Path, codon_to_aa: dict[str]) -> None:
+                              assembly_fasta:Path, aa_fasta: Path, codon_to_aa: dict[str],
+                              cds_gene_mapping: Union[None, dict[str]]) -> None:
         print("Starting cds extraction, aa translation, and aa writing\n(This may take awhile)")
 
         with aa_fasta.open("w") as aa_outhandle:
@@ -200,7 +223,11 @@ class CdsAaFastaManager:
                         aa_seq = cls.translate_cds_to_aa(cds_seq, codon_to_aa)
                     except KeyError:
                         continue
-                    aa_outhandle.write(f">{cds_id}\n")
+                    if cds_gene_mapping:
+                        gene = cds_gene_mapping[cds_id]
+                        aa_outhandle.write(f">{cds_id};{gene}\n")
+                    else:
+                        aa_outhandle.write(f">{cds_id}\n")
                     aa_outhandle.write(f"{aa_seq}\n")
 
     @classmethod
@@ -215,7 +242,8 @@ class CdsAaFastaManager:
 
     @classmethod
     def extract_and_write_cds_aa(cls, transcript_cds_id_mapping: dict[list[str]], cds_positions: dict[Any],
-                              assembly_fasta:Path, cds_fasta:Path, aa_fasta: Path, codon_to_aa: dict[str]) -> None:
+                              assembly_fasta:Path, cds_fasta:Path, aa_fasta: Path, codon_to_aa: dict[str],
+                              cds_gene_mapping: Union[None, dict[str]]) -> None:
         print("Starting cds extraction, aa translation, and cds and aa writing\n(This may take awhile)")
 
         with cds_fasta.open("w") as cds_outhandle, aa_fasta.open("w") as aa_outhandle:
@@ -230,13 +258,21 @@ class CdsAaFastaManager:
 
                 for cds_id in cds_ids:
                     cds_seq = cls.extract_cds_sequence(cds_positions[cds_id], transcript_seq)
-                    cds_outhandle.write(f">{cds_id}\n")
+                    if cds_gene_mapping:
+                        gene = cds_gene_mapping[cds_id]
+                        cds_outhandle.write(f">{cds_id};{gene}\n")
+                    else:
+                        cds_outhandle.write(f">{cds_id}\n")
                     cds_outhandle.write(f"{cds_seq}\n")
                     try:
                         aa_seq = cls.translate_cds_to_aa(cds_seq, codon_to_aa)
                     except KeyError:
                         continue
-                    aa_outhandle.write(f">{cds_id}\n")
+                    if cds_gene_mapping:
+                        gene = cds_gene_mapping[cds_id]
+                        aa_outhandle.write(f">{cds_id};{gene}\n")
+                    else:
+                        aa_outhandle.write(f">{cds_id}\n")
                     aa_outhandle.write(f"{aa_seq}\n")
 
 if __name__ == "__main__":
@@ -247,6 +283,7 @@ if __name__ == "__main__":
     parser.add_argument("-extraction_fields", type=str, required=True)
     parser.add_argument("-cds_fasta", type=str, required=False)
     parser.add_argument("-aa_fasta", type=str, required=False)
+    parser.add_argument("-add_gene_name", action="store_true", required=False)
     args = parser.parse_args()
 
     cds_fasta_path = Path(args.cds_fasta) if args.cds_fasta else None
@@ -255,5 +292,5 @@ if __name__ == "__main__":
     cafm = CdsAaFastaManager(Path(args.assembly_fasta), Path(args.transcript_metadata), Path(args.cds_metadata),
                              Path(args.extraction_fields), cds_fasta_path, aa_fasta_path)
     print("\nStarting CDS/AA Fasta Manager")
-    cafm.run(CODON_TO_AMINO_ACID)
+    cafm.run(CODON_TO_AMINO_ACID, args.add_gene_name)
     print("\nFinished")
