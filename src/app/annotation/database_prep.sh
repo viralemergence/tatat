c87fa5b14521e5c88f1ed9ee2ed1f49c19f3fe47
Brown_ Alexander James <alexander.brown@login-p3n01.kamiak.wsu.edu>
2025-04-16 20:30:39 -0700

    feat: added code for ncbi nt blastdb pull

--- src/app/annotation/database_prep.sh
+++ src/app/annotation/database_prep.sh
@@ -13,4 +13,9 @@ make_protein_blast_db() {
 	makeblastdb -in $1 -dbtype prot -title $2 -parse_seqids -blastdb_version 5 -taxid $3 -out $4
 }
 
+pull-nt-database() {
+	cd $BLASTDB \
+	&& perl "/src/tools/ncbi-blast-2.16.0+/bin/update_blastdb.pl" --passive --decompress nt
+}
+
 "$@"
